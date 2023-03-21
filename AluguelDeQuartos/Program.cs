e16ff17cccff92feac247495d62d6c0bb5fdc3d7
Matheus Souza <Matheus Souza@DESKTOP-Q6JOLM3>
2023-03-21 16:13:47 -0300

    Adicionar arquivos de projeto.

---
+++ AluguelDeQuartos/Program.cs
@@ -0,0 +1,54 @@
+﻿using AluguelDeQuartos;
+using System;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+           
+            bool IsSair = false;
+            while (!IsSair) {
+                Console.WriteLine("Quantos quartos serão alugados?");
+                int n = int.Parse(Console.ReadLine());
+
+                Quarto[] VQuartos = new Quarto[n];
+
+                Console.WriteLine("Selecione:\n1.Preencher Quartos\n2.Gerar Relatório");
+                int valor = int.Parse(Console.ReadLine());
+                if (valor == 1) {
+                    for (int i = 0; i < VQuartos.Length; i++) {
+                        Console.WriteLine($"Quartos Ocupados: \n" +
+                        $"Aluguel #{i + 1}:\n" +
+                        $"Name:");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Email:");
+                        string email = Console.ReadLine();
+
+                        Console.WriteLine("Quarto: ");
+                        int room = int.Parse(Console.ReadLine());
+
+                        VQuartos[i] = new Quarto((i + 1), name, email, room);
+
+                    }
+                }
+                else {                    
+                        for (int i = 0; i < VQuartos.Length; i++) {
+                            VQuartos[i].GetQuarto();
+                        }
+                   
+                }                
+               
+                Console.WriteLine("Deseja sair? \nS\nou\nN");
+                if (Console.ReadLine() == "S") 
+                        IsSair  = true;
+            }
+
+
+        }
+    }
+
+}
---
+++ AluguelDeQuartos/quarto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelDeQuartos {
+    internal class Quarto {
+        private int _id = 0;
+        private string _name = "";
+        private string _email = "";
+        private int _room = 0;
+
+        public Quarto(int id, string name, string email, int room) {
+            _id = id;
+            _name = name;
+            _email = email;
+            _room = room;
+        }
+        public void GetQuarto() {
+            Console.WriteLine($"Quartos Ocupados: \n" +
+                $"Aluguel #{_id}:\n" +
+                $"Name: {_name}\n" +
+                $"Email: {_email}\n" +
+                $"Quarto: {_room}");
+        }
+    }
+}
